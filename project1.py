3ef09098038656b29fbc3c84d05346afa93a6cbf
haniyetebyanian <h.tebyanian8374@gmail.com>
2025-01-01 12:52:44 -0500

    save a states of the disjointed string

--- project1.py
+++ project1.py
@@ -1,21 +1,26 @@
-# DM finally Project by Zeynab Golchin & Haniye Tebyanian by 
+# DM finally Project by Zeynab Golchin & Haniye Tebyanian 
 
 def partition_string (string, current = []) :
     if not string :
-        print(current)
-        return
+        return [current]
 
+    save_current = []
     for i in range(1, len(string) + 1) :
         prefix = string[:i] 
         supplement = string[i:]
 
         if len(prefix) <= 3 :
-            partition_string(supplement, current + [prefix])
+            save_current.extend(partition_string(supplement, current + [prefix]))
+
+    return save_current        
             
 
 
 
 
 binary = "0101010"
-partition_string(binary)        
+out = partition_string(binary)     
+for i in out :
+    print(i)  
+#print(out) 
 
