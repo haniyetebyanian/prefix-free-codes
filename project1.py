ea790e472309704712b1013a7c7cfdd835c0af61
haniyetebyanian <h.tebyanian8374@gmail.com>
2025-01-01 12:23:44 -0500

    the first vision to find the all state of the string that <= 3 without other conditions

--- project1.py
+++ project1.py
@@ -1,1 +1,19 @@
-# DM finally Project by Zeynab Golchin & Haniye Tebyanian by check
+# DM finally Project by Zeynab Golchin & Haniye Tebyanian by 
+
+def partition_string (string, current = []) :
+    if not string :
+        print(current)
+        return
+
+    for i in range(1, len(string) + 1) :
+        prefix = string[:i] 
+        supplement = string[i:]
+
+        if len(prefix) <= 3 :
+            partition_string(supplement, current + [prefix])
+            
+
+
+binary = "0101010"
+partition_string(binary)        
+
